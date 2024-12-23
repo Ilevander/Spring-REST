306cf68405a34a452cf2b57c15e51c49d38b49e2
dell <dell@DESKTOP-4P8U8AR>
2024-12-24 00:16:18 +0100

    Deployement: Creating Multi-Profils to run application in diffirent moods on different ports - mvn clean,mvn install and avoiding special caracters to override them BY UTF-8 Encoding , mvn install to generate JAR application - Deployed successfully - Generating WAR application by allowing packaaging on pom and some code customization on the main application starter

--- src/main/java/com/ilyass/SpringREST/SpringRestApplication.java
+++ src/main/java/com/ilyass/SpringREST/SpringRestApplication.java
@@ -2,14 +2,20 @@ package com.ilyass.SpringREST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringRestApplication {
+public class SpringRestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestApplication.class, args);
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringRestApplication.class, args);
+//	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringRestApplication.class);
 	}
-
 }
 
 /*
